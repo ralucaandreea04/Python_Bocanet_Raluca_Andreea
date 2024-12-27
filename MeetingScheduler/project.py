1d2c087f69796d3c0f1c8814883b9f83c391fcf0
ralucaandreea04 <ralucaandreea313@gmail.com>
2024-12-27 11:57:18 +0200

    Project Python
    Am facut logica pentru logarea unei persoane si posibilitatea acesteia de a vedea sedintele sale. Pentru butonul inserare informatii am realizat : interfata si logica pentru adaugarea unei persoane, stabilirea unei sedinte si vizualizarea sedintelor intr-un interval de timp. Am adaugat butonul pentru import/export insa nu este implementat.

--- MeetingScheduler/project.py
+++ MeetingScheduler/project.py
@@ -1,5 +1,6 @@
 import psycopg2
 import tkinter as tk
+from tkinter import ttk, messagebox
 import tkinter.font as tkFont
 
 def conectare_baza_de_date():
@@ -30,6 +31,50 @@ def executare_sql_script(file_path):
     except psycopg2.Error as e:
         print(f"eroare la executare: {e}")
 
+def adauga(root):
+    fereastra_adauga = tk.Toplevel(root)
+    fereastra_adauga.title("Fereastra de Adaugare Persoana")
+    fereastra_adauga.geometry("600x600")
+    fereastra_adauga.configure(bg="#fef5e7")
+    default_font = tkFont.Font(family="Bookman Old Style", size=12)
+    root.option_add("*Font", default_font)
+
+    main_frame = tk.Frame(fereastra_adauga, bg="#fef5e7")
+    main_frame.pack(expand=True, fill='both') 
+    main_frame.place(relx=0.5, rely=0.5, anchor='center')
+
+    label_adaugare = tk.Label(main_frame, text="Introduceti datele:", 
+                            font=("Bookman Old Style", 16), bg="#fef5e7")
+    label_adaugare.pack(pady=20)
+
+    label_prenume = tk.Label(main_frame, text="Prenume:", bg="#fef5e7",
+                             anchor='center', justify='center')
+    label_prenume.pack(pady=5)
+    entry_prenume = tk.Entry(main_frame, width=30, justify='center')
+    entry_prenume.pack(pady=5)
+
+    label_nume = tk.Label(main_frame, text="Nume:", bg="#fef5e7",
+                          anchor='center', justify='center')
+    label_nume.pack(pady=5)
+    entry_nume = tk.Entry(main_frame, width=30, justify='center')
+    entry_nume.pack(pady=5)
+
+    label_email = tk.Label(main_frame, text="Email:", bg="#fef5e7", 
+                           anchor='center', justify='center')
+    label_email.pack(pady=5)
+    entry_email = tk.Entry(main_frame, width=30, justify='center')
+    entry_email.pack(pady=5)
+
+    button_adaugare = tk.Button(main_frame, text="Adaugare", width=20,
+                                       command=lambda:adaugare_participant(
+                                           entry_prenume.get(),
+                                           entry_nume.get(),
+                                           entry_email.get()
+                                        )
+                                       )
+    button_adaugare.pack(pady=10)
+    button_adaugare.configure(bg="#f6ddcc")
+
 def adaugare_participant(nume, prenume, email):
     try:
         conn = conectare_baza_de_date()
@@ -43,7 +88,11 @@ def adaugare_participant(nume, prenume, email):
             (nume, prenume, email)
         )
         conn.commit()
-
+        if cursor.rowcount > 0:
+             messagebox.showinfo("Succes",
+                f"Participantul a fost adaugat cu succes!")
+        else:
+            messagebox.showerror("Eroare", "Participantul nu a fost adaugat!")
     except psycopg2.Error as e:
         print(f"eroare la adaugare: {e}")
 
@@ -67,33 +116,130 @@ def adaugare_sedinta(data_inceput, data_sfarsit,descriere,lista_participanti):
         )
         sedinta_id = cursor.fetchone()[0]
 
-        for participant_id in lista_participanti:
+        for participant in lista_participanti.split(","):
+            participant_prenume, participant_nume = participant.strip().split()
             cursor.execute(
                 """
-                INSERT INTO meeting_participants (meeting_id, person_id) 
-                VALUES (%s, %s)"
-                """, 
-                (sedinta_id, participant_id)
+                SELECT id FROM people
+                WHERE first_name = %s AND last_name = %s
+                """,
+                (participant_prenume, participant_nume)
             )
 
+            participant_id = cursor.fetchone()
+            if participant_id:
+                participant_id = participant_id[0]
+                cursor.execute(
+                    """
+                    INSERT INTO meeting_participants (meeting_id, person_id)
+                    VALUES (%s, %s)
+                    """,
+                    (sedinta_id, participant_id)
+                )
+            else:
+                messagebox.showerror(
+                    "Eroare", 
+                    f"Participantul {participant} nu exista!"
+                    )
         conn.commit()
+        if cursor.rowcount > 0:
+            messagebox.showinfo("Succes","Sedinta a fost adaugata cu succes!")
+        else:
+            messagebox.showerror("Eroare", "Sedinta nu a fost adaugata!")
 
     except psycopg2.Error as e:
         print(f"eroare la adaugare: {e}")
 
-def afisare_sedinte():
+def inserare_sedinta(root):
+    fereastra_inserare = tk.Toplevel(root)
+    fereastra_inserare.title("Fereastra de Adaugare Sedinta")
+    fereastra_inserare.geometry("600x600")
+    fereastra_inserare.configure(bg="#fef5e7")
+    default_font = tkFont.Font(family="Bookman Old Style", size=12)
+    root.option_add("*Font", default_font)
+
+    main_frame = tk.Frame(fereastra_inserare, bg="#fef5e7")
+    main_frame.pack(expand=True, fill='both') 
+    main_frame.place(relx=0.5, rely=0.5, anchor='center')
+
+    label_sedinta = tk.Label(main_frame, text="Introduceti datele:", 
+                            font=("Bookman Old Style", 16), bg="#fef5e7")
+    label_sedinta.pack(pady=20)
+
+    label_descriere = tk.Label(main_frame, text="Descriere:", bg="#fef5e7",
+                             anchor='center', justify='center')
+    label_descriere.pack(pady=5)
+    entry_descriere = tk.Entry(main_frame, width=30, justify='center')
+    entry_descriere.pack(pady=5)
+
+    label_data_ora_inceput= tk.Label(main_frame, 
+                                     text="Data si ora de inceput:", 
+                                     bg="#fef5e7",
+                                     anchor='center', 
+                                     justify='center'
+                                    )
+    label_data_ora_inceput.pack(pady=5)
+    entry_data_ora_inceput = tk.Entry(main_frame, width=30, justify='center')
+    entry_data_ora_inceput.pack(pady=5)
+
+    label_data_ora_sfarsit= tk.Label(main_frame, 
+                                     text="Data si ora de incheiere:", 
+                                     bg="#fef5e7",
+                                     anchor='center', 
+                                     justify='center'
+                                    )
+    label_data_ora_sfarsit.pack(pady=5)
+    entry_data_ora_sfarsit = tk.Entry(main_frame, width=30, justify='center')
+    entry_data_ora_sfarsit.pack(pady=5)
+
+    label_participanti = tk.Label(main_frame, 
+                                  text="Participanti:", 
+                                  bg="#fef5e7",
+                                  anchor='center', 
+                                  justify='center'
+                                )
+    label_participanti.pack(pady=5)
+    entry_participanti = tk.Entry(main_frame, width=30, justify='center')
+    entry_participanti.pack(pady=5)
+
+    button_adaugare = tk.Button(
+                                main_frame, 
+                                text="Adaugare", 
+                                width=20,
+                                command=lambda:adaugare_sedinta(
+                                    entry_data_ora_inceput.get(),
+                                    entry_data_ora_sfarsit.get(),
+                                    entry_descriere.get(),
+                                    entry_participanti.get()
+                                )
+                            )
+    button_adaugare.pack(pady=10)
+    button_adaugare.configure(bg="#f6ddcc")
+
+
+def afisare_sedinte(prenume, nume, email):
     conn=conectare_baza_de_date()
     cursor = conn.cursor()
-    cursor.execute("SELECT * FROM meetings")
-    print("Sedintele sunt:")
-    print(cursor.fetchall())
+    cursor.execute(
+        """
+        SELECT m.id, m.start_time, m.end_time, m.description
+        FROM meetings m
+        JOIN meeting_participants mp
+        ON m.id = mp.meeting_id
+        JOIN people p
+        ON p.id = mp.person_id
+        WHERE p.first_name = %s AND p.last_name = %s AND p.email = %s
+        """,
+        (prenume, nume, email)
+    )
+    return cursor.fetchall()
 
 def afisare_participanti_sedinta(sedinta_id):
     conn=conectare_baza_de_date()
     cursor = conn.cursor()
     cursor.execute(
         """
-        SELECT p.first_name, p.last_name, p.email
+        SELECT p.first_name, p.last_name
         FROM people p
         JOIN meeting_participants mp
         ON p.id = mp.person_id
@@ -101,8 +247,11 @@ def afisare_participanti_sedinta(sedinta_id):
         """,
         (sedinta_id,)
     )
-    print(f"Participanti pentru sedinta {sedinta_id}:")
-    print(cursor.fetchall())
+    participanti = cursor.fetchall()
+    lista= []
+    for participant in participanti:
+        lista.append(f"{participant[0]} {participant[1]}")
+    return ", ".join(lista)
 
 def afisarea_sedintelor_timp(data_inceput, data_sfarsit):
     conn=conectare_baza_de_date()
@@ -113,6 +262,204 @@ def afisarea_sedintelor_timp(data_inceput, data_sfarsit):
     )
     print("Sedintele sunt:")
     print(cursor.fetchall())
+
+def test_credentiale_logare(prenume, nume, email):
+    conn=conectare_baza_de_date()
+    cursor = conn.cursor()
+    cursor.execute(
+        """
+        SELECT * FROM people
+        WHERE first_name = %s AND last_name = %s AND email = %s
+        """,
+        (prenume, nume, email)
+    )
+    rezultat = cursor.fetchall()
+    if len(rezultat) > 0:
+        participant_logat(prenume, nume, email)
+    return "Datele introduse nu sunt corecte!"
+
+def participant_logat(prenume, nume, email):
+    fereastra = tk.Toplevel()
+    fereastra.title(f"Meeting Scheduler - {prenume} {nume}")
+    fereastra.geometry("600x600")
+    fereastra.configure(bg="#fef5e7")
+
+    label_bun_venit = tk.Label(
+        fereastra, 
+        text=f"Bun venit, {prenume}!", 
+        font=("Bookman Old Style", 16), 
+        bg="#fef5e7"
+    )
+    label_bun_venit.pack(pady=20)
+
+    label_sedinte = tk.Label(
+        fereastra, 
+        text="Sedintele tale programate:", 
+        font=("Bookman Old Style", 13), 
+        bg="#fef5e7",
+    )
+    label_sedinte.pack(pady=10)
+
+    frame_tabel = tk.Frame(fereastra, bg="#fef5e7")
+    frame_tabel.pack(pady=10)
+
+    style = ttk.Style()
+    style.configure(
+        "mystyle.Treeview", 
+        font=("Bookman Old Style", 8),
+        rowheight=30, 
+        background="#fef5e7",
+        fieldbackground="#fef5e7" 
+    )
+    style.configure(
+        "mystyle.Treeview.Heading", 
+        font=("Bookman Old Style", 14, "bold"), 
+        background="#f6ddcc", 
+        foreground="#4a4a4a"
+    )
+
+    tabel = ttk.Treeview(
+            frame_tabel, 
+            columns=("Interval", "Descriere"), 
+            show="headings", 
+            height=5,
+            style="mystyle.Treeview"
+            )
+    tabel.heading("Interval", text="Interval")
+    tabel.heading("Descriere", text="Descriere")
+    tabel.column("Interval", anchor="center", width=275)
+    tabel.column("Descriere", anchor="center", width=275)
+    tabel.pack(pady=10)
+
+    lista_sedinte = afisare_sedinte(prenume, nume, email)
+    if lista_sedinte:
+        for sedinta in lista_sedinte:
+            interval = f"{sedinta[1]} - {sedinta[2]}"
+            tabel.insert("", "end", values=(interval, sedinta[3]))
+    else:
+        tabel.insert("", "end", values=("Nu sunt sedinte", "", ""))
+
+    button_inchide = tk.Button(
+        fereastra, 
+        text="Inchide", 
+        command=fereastra.destroy, 
+        bg="#f6ddcc", 
+        width=20
+    )
+    button_inchide.pack(pady=20)
+
+def afisare_orar(data_inceput, data_sfarsit):
+    conn=conectare_baza_de_date()
+    cursor = conn.cursor()
+    cursor.execute(
+        "SELECT * FROM meetings WHERE start_time >= %s AND end_time <= %s", 
+        (data_inceput, data_sfarsit)
+    )
+    return cursor.fetchall()
+
+def afisare_sedinte_orar(data_inceput, data_sfarsit):
+    fereastra = tk.Toplevel()
+    fereastra.title(f"Meeting Scheduler - {data_inceput} - {data_sfarsit}")
+    fereastra.geometry("850x600")
+    fereastra.configure(bg="#fef5e7")
+
+    label_sedinte = tk.Label(
+        fereastra, 
+        text="Sedintele programate sunt:", 
+        font=("Bookman Old Style", 13), 
+        bg="#fef5e7",
+    )
+    label_sedinte.pack(pady=10)
+
+    frame_tabel = tk.Frame(fereastra, bg="#fef5e7")
+    frame_tabel.pack(pady=10)
+
+    style = ttk.Style()
+    style.configure(
+        "mystyle.Treeview", 
+        font=("Bookman Old Style", 9),
+        rowheight=40, 
+        background="#fef5e7",
+        fieldbackground="#fef5e7" 
+    )
+    style.configure(
+        "mystyle.Treeview.Heading", 
+        font=("Bookman Old Style", 14, "bold"), 
+        background="#f6ddcc", 
+        foreground="#4a4a4a"
+    )
+
+    tabel = ttk.Treeview(
+            frame_tabel, 
+            columns=("Interval", "Descriere","Participanti"), 
+            show="headings", 
+            height=5,
+            style="mystyle.Treeview"
+            )
+    tabel.heading("Interval", text="Interval")
+    tabel.heading("Descriere", text="Descriere")
+    tabel.heading("Participanti", text="Participanti")
+    tabel.column("Interval", anchor="center", width=275)
+    tabel.column("Descriere", anchor="center", width=275)
+    tabel.column("Participanti", anchor="center", width=275)
+    tabel.pack(pady=10)
+
+    lista_sedinte = afisare_orar(data_inceput, data_sfarsit)
+    if lista_sedinte:
+        for sedinta in lista_sedinte:
+            interval = f"{sedinta[1]} - {sedinta[2]}"
+            participanti = afisare_participanti_sedinta(sedinta[0])
+            tabel.insert("", "end", 
+                         values=(interval, sedinta[3], participanti)
+                        )
+
+    button_inchide = tk.Button(
+        fereastra, 
+        text="Inchide", 
+        command=fereastra.destroy, 
+        bg="#f6ddcc", 
+        width=20
+    )
+    button_inchide.pack(pady=20)
+
+def date_sedinte(root):
+    fereastra_sedinte = tk.Toplevel(root)
+    fereastra_sedinte.title("Date Sedinte")
+    fereastra_sedinte.geometry("600x600")
+    fereastra_sedinte.configure(bg="#fef5e7")
+    default_font = tkFont.Font(family="Bookman Old Style", size=12)
+    root.option_add("*Font", default_font)
+
+    main_frame = tk.Frame(fereastra_sedinte, bg="#fef5e7")
+    main_frame.pack(expand=True, fill='both') 
+    main_frame.place(relx=0.5, rely=0.5, anchor='center')
+
+    label_sedinte = tk.Label(main_frame, text="Introduceti orele dorite:", 
+                            font=("Bookman Old Style", 16), bg="#fef5e7")
+    label_sedinte.pack(pady=20)
+
+    label_inceput = tk.Label(main_frame, text="Data la care a inceput:", 
+                             bg="#fef5e7", anchor='center', justify='center')
+    label_inceput.pack(pady=5)
+    entry_inceput = tk.Entry(main_frame, width=30, justify='center')
+    entry_inceput.pack(pady=5)
+
+    label_sfarsit = tk.Label(main_frame, text="Data la care s-a terminat:", 
+                          bg="#fef5e7", anchor='center', justify='center')
+    label_sfarsit.pack(pady=5)
+    entry_sfarsit = tk.Entry(main_frame, width=30, justify='center')
+    entry_sfarsit.pack(pady=5)
+
+    button_sedinte = tk.Button(main_frame, 
+                                       text="Vizualizare", 
+                                       width=20,
+                                       command=lambda: afisare_sedinte_orar(
+                                        entry_inceput.get(),
+                                        entry_sfarsit.get(),
+                                        )
+                                    )   
+    button_sedinte.pack(pady=10)
+    button_sedinte.configure(bg="#f6ddcc")
 
 def main():
     print("Persoanele sunt:")
@@ -154,7 +501,12 @@ def logare(root):
     entry_email = tk.Entry(main_frame, width=30, justify='center')
     entry_email.pack(pady=5)
 
-    button_logare_secundar = tk.Button(main_frame, text="Logare", width=20)
+    button_logare_secundar = tk.Button(main_frame, text="Logare", width=20,
+                                       command=lambda:test_credentiale_logare(
+                                        entry_prenume.get(),entry_nume.get(),
+                                        entry_email.get()
+                                        )
+                                       )
     button_logare_secundar.pack(pady=10)
     button_logare_secundar.configure(bg="#f6ddcc")
 
@@ -175,15 +527,29 @@ def inserare_informatii(root):
     label_informatii.pack(pady=20)
 
     button_adauga_persoana = tk.Button(main_frame, text="Adauga persoana",
-                                       bg="#f6ddcc", width=20)
+                                       bg="#f6ddcc", width=20, 
+                                       command=lambda: adauga(root))
     button_adauga_persoana.pack(pady=10)
 
     button_stabileste_sedinta = tk.Button(main_frame,
-                        text="Stabileste o sedinta", bg="#f6ddcc", width=20)
+                        text="Stabileste o sedinta", 
+                        bg="#f6ddcc", 
+                        width=20,
+                        command=lambda: inserare_sedinta(root)
+                        )
     button_stabileste_sedinta.pack(pady=10)
 
-    button_vezi_sedinte = tk.Button(main_frame, text="Vezi sedintele", 
+    button_import_export = tk.Button(main_frame,
+                                    text="Import/Export calendar",
                                     bg="#f6ddcc", width=20)
+    button_import_export.pack(pady=10)
+
+    button_vezi_sedinte = tk.Button(main_frame, 
+                                    text="Vezi sedintele", 
+                                    bg="#f6ddcc", 
+                                    width=20,
+                                    command=lambda: date_sedinte(root),
+                                    )
     button_vezi_sedinte.pack(pady=10)
 
 def creeaza_interfata():
